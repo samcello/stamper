78bad4e1dd89e3e76d85f8f67e069b0331c7eec6
Jerry <31403436@qq.com>
2019-01-22 00:04:34 +0800

    1

--- server/controllers/openid.js
+++ server/controllers/openid.js
@@ -1,7 +1,7 @@
 const userModel = require('../model/user.js')
 const config = require('../config')
 const _ = require('lodash');
-const request = require("request");
+const request = require("request-promise");
 module.exports = async (ctx, next) => {
     let { code } = ctx.query;
     let appId = config.appId;
@@ -10,13 +10,6 @@ module.exports = async (ctx, next) => {
     let opts = {
       url: `https://api.weixin.qq.com/sns/jscode2session?appid=${appId}&secret=${secret}&js_code=${js_code}&grant_type=authorization_code`
     }
-    request(opts, function (error, response, body) {
-        console.log('error:', error); // Print the error if one occurred
-        console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-        console.log('body:', body); // Print the HTML for the Google homepage.
-        if(response.statusCode == 200){
-            ctx.body = body;
-        }
-    }); 
+    await request(opts).then((repos)=>{ctx.body = repos.body}); 
 }
 
